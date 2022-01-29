38f4a0b5afda170896d470d2638cb32fefc4b353
DanielSemeniuk <91903603+DanielSemeniuk@users.noreply.github.com>
2022-01-28 19:55:57 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,49 @@
+// Daniel Semeniuk, 101368587, January 28th 2022
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	while (1)
+	{
+		cout << "	|/" << endl;
+		cout << "	00" << endl;
+		cout << "~000000000";
+
+		Sleep(1000);
+		system("CLS");
+
+		cout << "		|/" << endl;
+		cout << "	     0  00" << endl;
+		cout << "	~0000 0000";
+
+		Sleep(1000);
+		system("CLS");
+
+		cout << "			|/" << endl;
+		cout << "		    000 00" << endl;
+		cout << "		~000   000";
+
+		Sleep(1000);
+		system("CLS");
+
+		cout << "				|/" << endl;
+		cout << "			     0  00" << endl;
+		cout << "			~0000 0000";
+
+		Sleep(1000);
+		system("CLS");
+
+		cout << "					|/" << endl;
+		cout << "					00" << endl;
+		cout << "				~000000000";
+
+		Sleep(1000);
+		system("CLS");
+	}
+	
+
+	return 0;
+}
